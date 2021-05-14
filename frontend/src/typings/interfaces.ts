1241607d59dcf4b9aba6ca3653c5afbb37d21a76
Shair <akhkhdilavar@edu.hse.ru>
2021-05-14 14:32:41 +0300

    interfaces

--- frontend/src/typings/interfaces.ts
+++ frontend/src/typings/interfaces.ts
@@ -1,5 +1,71 @@
 export interface IUserState {
-  authorized: boolean;
-  username: string;
-  adminRole: boolean;
+    authorized: boolean;
+    username: string;
+    adminRole: boolean;
+}
+
+// для каждой таблицы сущности
+// например, для inventoryitem интерфейс {}
+enum itemCategory {
+    'МОНОБЛОК',
+    'ПРИНТЕР',
+    'НОУТБУК',
+    'КОМПЬЮТЕР',
+    'МЕБЕЛЬ'
+}
+
+enum userType {
+    'ADMIN',
+    'READER'
+}
+
+enum inquiryStatus {
+    'new',
+    'queued',
+    'finished'
+}
+
+export interface IInventoryItem {
+    id: number;
+    invid: string;
+    category: itemCategory;
+    displayName: string;
+    serialNum: string;
+    price: number;
+    available: boolean;
+}
+
+export interface IInquiry {
+    id: number;
+    inquirerName: string;
+    inquirerEmail: string;
+    comment: string;
+    status: inquiryStatus;
+    invid: IInventoryItem["invid"];
+    itemName: IInventoryItem["displayName"];
+}
+
+export interface ILog {
+    id: number;
+    itemId: IInventoryItem['invid'];
+    timestamp: Date;
+    description: string;
+    author: IUser;
+}
+
+export interface IUser {
+    id: number;
+    userType: userType;
+    displayName: string;
+}
+
+export interface IFile {
+    id: number;
+    fileName: string;
+}
+
+export interface requestResponse {
+    success: boolean;
+    errorMessage?: string;
+    data?: any;
 }
